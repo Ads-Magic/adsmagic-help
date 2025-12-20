bb377024311ca1c835d1db7f19ced37af48dbf63
Seiffeddeen Keilani <keilaniswk@gmail.com>
2025-12-20 02:42:54 +0200

    Disable 'edit this page' links in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,8 +56,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Ads-Magic/adsmagic-help/tree/main/',
+          // editUrl:
+          // 'https://github.com/Ads-Magic/adsmagic-help/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -67,8 +67,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Ads-Magic/adsmagic-help/tree/main/',
+          // editUrl:
+          // 'https://github.com/Ads-Magic/adsmagic-help/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
